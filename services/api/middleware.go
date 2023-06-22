605463d32d512954281844a57675b5a90a08300c
Dan M <dan@marrera.com>
2023-06-22 06:53:45 -0500

    chore: remove commented out code

--- services/api/middleware.go
+++ services/api/middleware.go
@@ -10,32 +10,9 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// AddRequestInfoToContext will extract key bits of information from the
-// request (i.e. a unique request-id, etc.) and add them to the context
-// so that they can be logged
-// func AddRequestInfoToContext(appCtx shared.ApplicationContext) mux.MiddlewareFunc {
-// 	return func(h http.Handler) http.Handler {
-// 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-// 			reqId := r.Header.Get(HeaderRequestId)
-// 			if reqId == "" {
-// 				reqId = uuid.New().String()
-// 			}
-
-// 			appCtx.RootCtx = utils.AddMapToContext(appCtx.RootCtx, utils.FieldMap{
-// 				"request-url": r.URL.Path,
-// 				"request-id":  reqId,
-// 			})
-
-// 			appCtx.Logger.WithCtx(appCtx.RootCtx).Debugf("request received")
-// 			h.ServeHTTP(w, r)
-// 			appCtx.Logger.WithCtx(appCtx.RootCtx).Debugf("request completed")
-// 		})
-// 	}
-// }
-
 // MetricsMiddleware will integrate with the metrics collector service to create
 // and increment a standard set of obversability metrics for each, registered,
-// apo endpoint
+// api endpoint
 func MetricsMiddleware(appCtx shared.ApplicationContext) mux.MiddlewareFunc {
 	collector := appCtx.Collector
 
